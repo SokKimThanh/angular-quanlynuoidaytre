3ebddcc225ce47294e82852dc368d7d75861bf86
Sok Thanh <thanhsk1991@gmail.com>
2020-01-28 00:00:40 +0700

    Upload template Quan Ly Nuoi Day Tre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -10,5 +11,35 @@ import { ThongtintreComponent } from './thongtintre/thongtintre.component';
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, ThongtintreComponent ],
   bootstrap:    [ AppComponent ]
+=======
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    ScheduleComponent,
+    ContactComponent,
+    GalleryComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+>>>>>>> Upload template Quan Ly Nuoi Day Tre
 })
 export class AppModule { }
